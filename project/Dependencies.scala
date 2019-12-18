1b02fcf1e3f9880864f5c57f66c60fdc47c6c208
Scala Steward <me@scala-steward.org>
2019-12-18 20:46:19 +0100

    Update cats-core to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
-    val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
+    val effect = "org.typelevel" %% "cats-effect" % "2.1.0"
   }
 
   object Akka {
