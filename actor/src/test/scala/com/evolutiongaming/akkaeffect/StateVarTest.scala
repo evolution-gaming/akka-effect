bca341fbf1048935cd0e9b56d133aaf9f19adc1c
Yaroslav Klymko <t3hnar@gmail.com>
2020-01-23 12:16:45 +0100

    add StateVarTest

---
+++ actor/src/test/scala/com/evolutiongaming/akkaeffect/StateVarTest.scala
@@ -0,0 +1,54 @@
+package com.evolutiongaming.akkaeffect
+
+import cats.effect.IO
+import cats.implicits._
+import com.evolutiongaming.akkaeffect.IOSuite._
+import com.evolutiongaming.catshelper.CatsHelper._
+import com.evolutiongaming.catshelper.FromFuture
+import org.scalatest.Succeeded
+import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+import scala.concurrent.{Future, Promise, TimeoutException}
+import scala.util.control.NoStackTrace
+
+class StateVarTest extends AsyncFunSuite with Matchers {
+
+  test("success") {
+    val state = StateVar[IO].of(0)
+
+    def increment() = {
+      val promise = Promise[Int]
+      state { a =>
+        val b = a + 1
+        IO { promise.success(b) }.as(b)
+      }
+      promise.future
+    }
+
+    val futures = (1 to 100).map { expected =>
+      increment().map { _ shouldEqual expected }
+    }
+    Future
+      .sequence(futures)
+      .map { _ => Succeeded }
+  }
+
+  test("failure") {
+    val error = new RuntimeException with NoStackTrace
+    val state = StateVar[IO].of(())
+
+    state { _ => error.raiseError[IO, Unit] }
+
+    val promise = Promise[Unit]
+    state { _ => IO { promise.success(()) } }
+
+    FromFuture[IO]
+      .apply { promise.future }
+      .timeout(100.millis)
+      .attempt
+      .map { _ should matchPattern { case Left(_: TimeoutException) => } }
+      .toFuture
+  }
+}
