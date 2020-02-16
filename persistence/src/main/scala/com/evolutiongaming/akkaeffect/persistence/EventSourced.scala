7e9eea796751043e38ebdeac848593810a39d3d8
Yaroslav Klymko <t3hnar@gmail.com>
2020-02-16 18:50:40 +0100

    add Started

--- persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourced.scala
+++ persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourced.scala
@@ -5,6 +5,20 @@ import cats.Monad
 import cats.effect.Resource
 import cats.implicits._
 
+/**
+  * EventSourced describes lifecycle entity with regards to event sourcing
+  * Lifecycle phases:
+  *
+  * 1. Started: we have id in place and can decide whether we should continue with recovery
+  * 2. Recovering: reading snapshot and replaying events
+  * 3. Receiving: receiving commands and potentially storing events & snapshots
+  * 4. Terminating: triggers all release hooks of allocated resources within previous phases
+  *
+  * @tparam S snapshot
+  * @tparam C command
+  * @tparam E event
+  * @tparam R reply
+  */
 trait EventSourced[F[_], S, C, E, R] {
 
   def id: String
@@ -34,24 +48,21 @@ object EventSourced {
       e1f: E1 => F[E],
       rf: R => F[R1])(implicit
       F: Monad[F],
-    ): EventSourced[F, S1, C1, E1, R1] = {
+    ): EventSourced[F, S1, C1, E1, R1] = new EventSourced[F, S1, C1, E1, R1] {
 
-      new EventSourced[F, S1, C1, E1, R1] {
+      def id = self.id
 
-        def id = self.id
+      def recoveryStarted(snapshotOffer: Option[SnapshotOffer[S1]]) = {
 
-        def recoveryStarted(snapshotOffer: Option[SnapshotOffer[S1]]) = {
+        val snapshotOffer1 = snapshotOffer.traverse { offer =>
+          s1f(offer.snapshot).map { snapshot => offer.copy(snapshot = snapshot) }
+        }
 
-          val snapshotOffer1 = snapshotOffer.traverse { offer =>
-            s1f(offer.snapshot).map { snapshot => offer.copy(snapshot = snapshot) }
-          }
-
-          for {
-            snapshotOffer <- Resource.liftF(snapshotOffer1)
-            recovering    <- self.recoveryStarted(snapshotOffer)
-          } yield {
-            recovering.convert(sf, s1f, cf, ef, e1f, rf)
-          }
+        for {
+          snapshotOffer <- Resource.liftF(snapshotOffer1)
+          recovering    <- self.recoveryStarted(snapshotOffer)
+        } yield {
+          recovering.convert(sf, s1f, cf, ef, e1f, rf)
         }
       }
     }
---
+++ persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/Started.scala
@@ -0,0 +1,71 @@
+package com.evolutiongaming.akkaeffect.persistence
+
+import cats.Monad
+import cats.effect.Resource
+import cats.implicits._
+
+trait Started[F[_], S, C, E, R] {
+
+  def recoveryStarted(snapshotOffer: Option[SnapshotOffer[S]]): Resource[F, Recovering[F, S, C, E, R]]
+}
+
+object Started {
+
+  implicit class StartedOps[F[_], S, C, E, R](val self: Started[F, S, C, E, R]) extends AnyVal {
+
+    def convert[S1, C1, E1, R1](
+      sf: S => F[S1],
+      s1f: S1 => F[S],
+      cf: C1 => F[C],
+      ef: E => F[E1],
+      e1f: E1 => F[E],
+      rf: R => F[R1])(implicit
+      F: Monad[F],
+    ): Started[F, S1, C1, E1, R1] = {
+
+      snapshotOffer: Option[SnapshotOffer[S1]] => {
+
+        val snapshotOffer1 = snapshotOffer.traverse { offer =>
+          s1f(offer.snapshot).map { snapshot => offer.copy(snapshot = snapshot) }
+        }
+
+        for {
+          snapshotOffer <- Resource.liftF(snapshotOffer1)
+          recovering    <- self.recoveryStarted(snapshotOffer)
+        } yield {
+          recovering.convert(sf, s1f, cf, ef, e1f, rf)
+        }
+      }
+    }
+
+
+    def widen[S1 >: S, C1 >: C, E1 >: E, R1 >: R](
+      sf: S1 => F[S],
+      cf: C1 => F[C],
+      ef: E1 => F[E])(implicit
+      F: Monad[F],
+    ): Started[F, S1, C1, E1, R1] = {
+      snapshotOffer: Option[SnapshotOffer[S1]] => {
+
+        val snapshotOffer1 = snapshotOffer.traverse { snapshotOffer =>
+          sf(snapshotOffer.snapshot).map { snapshot => snapshotOffer.copy(snapshot = snapshot) }
+        }
+
+        for {
+          snapshotOffer <- Resource.liftF(snapshotOffer1)
+          recovering    <- self.recoveryStarted(snapshotOffer)
+        } yield {
+          recovering.widen(sf, cf, ef)
+        }
+      }
+    }
+
+
+    def typeless(
+      sf: Any => F[S],
+      cf: Any => F[C],
+      ef: Any => F[E])(implicit
+      F: Monad[F],
+    ): Started[F, Any, Any, Any, Any] = widen[Any, Any, Any, Any](sf, cf, ef)
+  }
+}
