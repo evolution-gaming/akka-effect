fe933882bffd1850d79e12df771da96829ef8092
Yaroslav Klymko <t3hnar@gmail.com>
2020-02-28 08:30:19 +0100

    improve AkkaEffectHelper

---
+++ actor/src/main/scala/com/evolutiongaming/akkaeffect/AkkaEffectHelper.scala
@@ -0,0 +1,40 @@
+package com.evolutiongaming.akkaeffect
+
+import cats.effect.Sync
+import cats.implicits._
+import com.evolutiongaming.catshelper.CatsHelper._
+import com.evolutiongaming.catshelper.{ApplicativeThrowable, FromFuture, ToFuture}
+
+import scala.reflect.ClassTag
+
+object AkkaEffectHelper {
+
+  implicit class IdOpsAkkaEffectHelper[A](val self: A) extends AnyVal {
+
+    def cast[F[_] : ApplicativeThrowable, B <: A](implicit tag: ClassTag[B]): F[B] = {
+      tag
+        .unapply(self)
+        .fold {
+          val error = new ClassCastException(s"${ self.getClass.getName } cannot be cast to ${ tag.runtimeClass.getName }")
+          error.raiseError[F, B]
+        } { a =>
+          a.pure[F]
+        }
+    }
+  }
+
+
+  implicit class OpsAkkaEffectHelper[F[_], A](val self: F[A]) extends AnyVal {
+
+    /**
+      * Unlike `Concurrent.start`, `startNow` tries to evaluate effect on current thread, unless it is asynchronous
+      *
+      * @return Outer F[_] is about launching effect, inner F[_] is about effect completed
+      */
+    def startNow(implicit F: Sync[F], toFuture: ToFuture[F], fromFuture: FromFuture[F]): F[F[A]] = {
+      Sync[F]
+        .delay { self.toFuture }
+        .map { future => fromFuture(future) }
+    }
+  }
+}
