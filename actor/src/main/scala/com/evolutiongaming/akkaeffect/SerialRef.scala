7b474039e9e55153a345912f484c2b497c9d3ab7
Yaroslav Klymko <t3hnar@gmail.com>
2020-02-28 08:29:34 +0100

    add SerialRef

---
+++ actor/src/main/scala/com/evolutiongaming/akkaeffect/SerialRef.scala
@@ -0,0 +1,67 @@
+package com.evolutiongaming.akkaeffect
+
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import cats.implicits._
+import com.evolutiongaming.catshelper.{FromFuture, ToFuture}
+
+
+/**
+  * Similar [[cats.effect.concurrent.Ref]], however supports effectful modification
+  */
+private[akkaeffect] trait SerialRef[F[_], A] {
+
+  def get: F[A]
+
+  def modify[B](f: A => F[(A, B)]): F[F[B]]
+
+  def update[B](f: A => F[A]): F[F[Unit]]
+}
+
+private[akkaeffect] object SerialRef {
+
+  def of[F[_] : Sync : ToFuture : FromFuture, A](a: A): F[SerialRef[F, A]] = {
+    for {
+      serial    <- Serial.of[F]
+      serialRef <- of(serial, a)
+    } yield serialRef
+  }
+
+
+  def of[F[_] : Sync, A](serial: Serial[F], a: A): F[SerialRef[F, A]] = {
+    Ref[F]
+      .of(a)
+      .map { ref =>
+        new SerialRef[F, A] {
+
+          def get = ref.get
+
+          def modify[B](f: A => F[(A, B)]) = {
+            serial {
+              for {
+                a     <- get
+                ab    <- f(a)
+                (a, b)  = ab
+                _      <- ref.set(a)
+              } yield b
+            }
+          }
+
+          def update[B](f: A => F[A]) = {
+            modify { a => f(a).map { a => (a, ()) } }
+          }
+        }
+      }
+  }
+
+  
+  def apply[F[_] : Sync]: ApplyBuilders[F] = new ApplyBuilders(Sync[F])
+
+
+  final class ApplyBuilders[F[_]](val F: Sync[F]) extends AnyVal {
+
+    def of[A](a: A)(implicit toFuture: ToFuture[F], fromFuture: FromFuture[F]): F[SerialRef[F, A]] = {
+      SerialRef.of(a)(F, toFuture, fromFuture)
+    }
+  }
+}
---
+++ actor/src/test/scala/com/evolutiongaming/akkaeffect/SerialRefTest.scala
@@ -0,0 +1,31 @@
+package com.evolutiongaming.akkaeffect
+
+import cats.effect.concurrent.Deferred
+import cats.effect.{Concurrent, IO}
+import cats.implicits._
+import com.evolutiongaming.akkaeffect.IOSuite._
+import com.evolutiongaming.catshelper.{FromFuture, ToFuture}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class SerialRefTest extends AnyFunSuite with Matchers {
+
+  test("update") {
+    update[IO].run()
+  }
+
+  private def update[F[_] : Concurrent : FromFuture : ToFuture]: F[Unit] = {
+    for {
+      serialRef <- SerialRef[F].of(List.empty[Int])
+      deferred  <- Deferred[F, Int]
+      _         <- serialRef.update { a => deferred.get.map { _ :: a } }
+      f         <- (1 to 9).toList.foldLeftM(().pure[F]) { case (_, a) => serialRef.update { as => (a :: as).pure[F] } }
+      a         <- serialRef.get
+      _          = a shouldEqual List.empty
+      _         <- deferred.complete(0)
+      _         <- f
+      a         <- serialRef.get
+      _          = a shouldEqual (0 to 9).toList
+    } yield {}
+  }
+}
