dfe1567e2f43767d0b2a9a36756396a508bec104
Yaroslav Klymko <t3hnar@gmail.com>
2020-03-01 00:42:16 +0100

    add Aggregate

---
+++ eventsourcing/src/main/scala/com/evolutiongaming/akkaeffect/eventsourcing/Aggregate.scala
@@ -0,0 +1,79 @@
+package com.evolutiongaming.akkaeffect.eventsourcing
+
+import cats.data.{NonEmptyList => Nel}
+import cats.effect.Concurrent
+import cats.effect.concurrent.{Deferred, Ref}
+import cats.effect.implicits._
+import cats.implicits._
+
+
+private[akkaeffect] trait Aggregate[F[_], S, A, B] {
+
+  def apply(a: A): F[F[B]]
+}
+
+private[akkaeffect] object Aggregate {
+
+  /**
+    * This runs f strictly serially and keeps stashing A meanwhile,
+    * when f done with previous batch, it will proceed with next one
+    */
+  def of[F[_] : Concurrent, S, A, B](
+    s: S)(
+    f: (S, Nel[A]) => F[(S, B)]
+  ): F[Aggregate[F, S, A, B]] = {
+
+    case class E(a: A, d: Deferred[F, Either[Throwable, B]])
+
+    Ref[F]
+      .of(s.asLeft[List[E]])
+      .map { ref =>
+
+        def start(s: S, e: E): F[Unit] = {
+          (s, Nel.of(e))
+            .tailRecM[F, Unit] { case (s, es0) =>
+              val es = es0.reverse
+              val as = es.map { _.a }
+              for {
+                t  <- f(s, as).attempt
+                s1  = t.fold(_ => s, { case (a, _) => a })
+                b   = t.map { case (_, b) => b }
+                _  <- es.foldMapM { _.d.complete(b) }
+                e  <- ref.modify {
+                  case Right(e :: es) => (List.empty[E].asRight, (s1, Nel(e, es)).asLeft)
+                  case _              => (s1.asLeft, ().asRight)
+                }
+              } yield e
+            }
+            .start
+            .void
+        }
+
+        (a: A) => {
+          for {
+            d <- Deferred.uncancelable[F, Either[Throwable, B]]
+            e  = E(a, d)
+            r <- ref.modify {
+              case Right(es) => ((e :: es).asRight, ().pure[F])
+              case Left(s)   => (List.empty[E].asRight, start(s, e))
+            }
+            _ <- r
+          } yield for {
+            b <- d.get
+            b <- b.liftTo[F]
+          } yield b
+        }
+      }
+  }
+
+
+  def apply[F[_] : Concurrent]: ApplyBuilders[F] = new ApplyBuilders(Concurrent[F])
+
+
+  final class ApplyBuilders[F[_]](val F: Concurrent[F]) extends AnyVal {
+
+    def of[S, A, B](s: S)(f: (S, Nel[A]) => F[(S, B)]): F[Aggregate[F, S, A, B]] = {
+      Aggregate.of(s)(f)(F)
+    }
+  }
+}
---
+++ eventsourcing/src/test/scala/com/evolutiongaming/akkaeffect/eventsourcing/AggregateTest.scala
@@ -0,0 +1,40 @@
+package com.evolutiongaming.akkaeffect.eventsourcing
+
+import cats.data.{NonEmptyList => Nel}
+import cats.effect.concurrent.Deferred
+import cats.effect.{Concurrent, IO}
+import cats.implicits._
+import com.evolutiongaming.akkaeffect.IOSuite._
+import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.matchers.should.Matchers
+
+
+class AggregateTest extends AsyncFunSuite with Matchers {
+
+  test("apply") {
+    apply[IO].run()
+  }
+
+  private def apply[F[_] : Concurrent]: F[Unit] = {
+    for {
+      serialBatch <- Aggregate[F].of(List.empty[Nel[Int]]) { (s, as: Nel[F[Int]]) =>
+        for {
+          as <- as.sequence
+        } yield {
+          val s1 = as :: s
+          (s1, s1)
+        }
+      }
+      d0 <- Deferred[F, Int]
+      _  <- serialBatch(d0.get)
+      _  <- serialBatch(1.pure[F])
+      d1 <- Deferred[F, Int]
+      _  <- serialBatch(d1.get)
+      a  <- serialBatch(3.pure[F])
+      _  <- d0.complete(0)
+      _  <- d1.complete(2)
+      a  <- a
+      _   = a shouldEqual List(Nel.of(1, 2, 3), Nel.of(0))
+    } yield {}
+  }
+}
