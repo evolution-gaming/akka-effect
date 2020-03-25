079d3cca8163ee54ec018dee9d8dede89721c16a
Yaroslav Klymko <t3hnar@gmail.com>
2020-03-25 21:23:40 +0100

    improve EventSourcedId

--- persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourcedId.scala
+++ persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourcedId.scala
@@ -1,6 +1,16 @@
 package com.evolutiongaming.akkaeffect.persistence
 
+import cats.implicits._
+import cats.{Order, Show}
+
 /**
   * @see [[akka.persistence.PersistentActor.persistenceId]]
   */
 final case class EventSourcedId(value: String)
+
+object EventSourcedId {
+
+  implicit val orderEventSourcedId: Order[EventSourcedId] = Order.by { a: EventSourcedId => a.value }
+
+  implicit val showEventSourcedId: Show[EventSourcedId] = Show.fromToString
+}
