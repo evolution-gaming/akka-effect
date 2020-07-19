562e3e89db17e3460327cc8093f0b656d47cb3b0
Yaroslav Klymko <t3hnar@gmail.com>
2020-07-19 17:13:53 +0300

    downgrade pureconfig

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "1.0.4"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
   val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.11.0"
-  val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.13.0"
+  val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
   val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "0.1.1"
 
   object Cats {
