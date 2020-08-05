1e3a8698f7a39cccc6976d552da09e95c5ebbe67
Scala Steward <me@scala-steward.org>
2020-08-05 16:18:13 +0200

    Update cats-helper to 2.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   
   val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.0"
-  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.0.4"
+  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.0.6"
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.2"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "1.0.5"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
