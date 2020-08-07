7b7edbe19d71b8a7a6ca71874bbd46e78a3c52ce
Scala Steward <me@scala-steward.org>
2020-08-07 06:45:45 +0200

    Update scalatest to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   
-  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.0"
+  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.1"
   val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.0.4"
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.2"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "1.0.5"
