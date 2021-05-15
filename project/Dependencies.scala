413faf2231228e4c488c635e89a8aa7c83690833
Scala Steward <me@scala-steward.org>
2021-05-15 14:20:23 +0200

    Update kind-projector to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.2"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "2.1.0"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
-  val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.11.3"
+  val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.13.0"
   val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
   val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "0.1.2"
 
