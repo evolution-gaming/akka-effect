5d432a97cccaab0ae432f85f29f7d88e4373d279
Scala Steward <me@scala-steward.org>
2021-10-05 04:43:53 +0200

    Update cats-effect to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   }
 
   object CatsEffect {
-    private val version = "2.5.4"
+    private val version = "3.2.9"
     val effect = "org.typelevel" %% "cats-effect" % version
   }
 
