06865d1dc2f20480b2f3beea5d154f91606ed0e3
Yaroslav Klymko <t3hnar@gmail.com>
2021-12-07 13:53:07 +0100

    add ClusterShardingTest

---
+++ cluster-sharding/src/test/scala/com/evolutiongaming/akkaeffect/cluster/sharding/ClusterShardingTest.scala
@@ -0,0 +1,73 @@
+package com.evolutiongaming.akkaeffect.cluster.sharding
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.cluster.sharding.ShardCoordinator.LeastShardAllocationStrategy
+import akka.cluster.sharding.ShardRegion.Msg
+import akka.cluster.sharding.ClusterShardingSettings
+import cats.effect.{IO, Resource}
+import cats.syntax.all._
+import com.typesafe.config.ConfigFactory
+import com.evolutiongaming.akkaeffect.{ActorRefOf, ActorSuite}
+import com.evolutiongaming.akkaeffect.IOSuite._
+import com.evolutiongaming.akkaeffect.persistence.TypeName
+import com.evolutiongaming.akkaeffect.testkit.Probe
+import com.evolutiongaming.catshelper.{Blocking, LogOf}
+import com.evolutiongaming.catshelper.CatsHelper._
+import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ClusterShardingTest extends AsyncFunSuite with ActorSuite with Matchers {
+
+  override def config = {
+    IO {
+      ConfigFactory
+        .load("ClusterShardingTest.conf")
+        .some
+    }
+  }
+
+  test("start") {
+    case object HandOffStopMessage
+
+    implicit val blocking = Blocking.fromExecutionContext(executor)
+
+    val result = for {
+      logOf                   <- LogOf.slf4j[IO].toResource
+      log                     <- logOf(classOf[ClusterShardingTest]).toResource
+      clusterSharding         <- ClusterSharding.of[IO](actorSystem)
+      clusterSharding         <- clusterSharding.withLogging(log).pure[Resource[IO, *]]
+      clusterShardingSettings <- IO { ClusterShardingSettings(actorSystem) }.toResource
+      actorRefOf               = ActorRefOf.fromActorRefFactory(actorSystem)
+      probe                   <- Probe.of(actorRefOf)
+      props                    = {
+        def actor() = new Actor {
+          def receive = {
+            case ()                 => sender().tell((), self)
+            case HandOffStopMessage => context.stop(self)
+          }
+        }
+
+        Props(actor())
+      }
+      shardRegion             <- clusterSharding.start(
+          TypeName("typeName"),
+          props,
+          clusterShardingSettings,
+          { case a => ("entityId", a) },
+          { (_: Msg) => "shardId" },
+          new LeastShardAllocationStrategy(1, 1),
+          HandOffStopMessage)
+    } yield {
+      for {
+        a <- probe.expect[Unit]
+        _ <- IO { shardRegion.tell((), probe.actorEffect.toUnsafe) }
+        a <- a
+      } yield {
+        a.msg
+      }
+    }
+    result
+      .use(identity)
+      .run()
+  }
+}
