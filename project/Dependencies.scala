6aaa7508494d1a3b946c19fc0f010c3a53ec5a29
Scala Steward <me@scala-steward.org>
2022-01-14 18:43:06 +0100

    Update retry to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.10"
   val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.7.2"
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.3"
-  val retry                       = "com.evolutiongaming"   %% "retry"                     % "2.1.1"
+  val retry                       = "com.evolutiongaming"   %% "retry"                     % "3.0.0"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
   val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.13.2"
   val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
