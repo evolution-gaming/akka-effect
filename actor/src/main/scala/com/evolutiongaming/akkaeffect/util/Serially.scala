bbdd8fd5aaf4f7683f42f5f1137a85d0557a9794
Z1kkurat <kshelopugin@gmail.com>
2022-08-02 14:43:47 +0200

    Fix Serially not handling many concurrent tasks

--- actor/src/main/scala/com/evolutiongaming/akkaeffect/util/Serially.scala
+++ actor/src/main/scala/com/evolutiongaming/akkaeffect/util/Serially.scala
@@ -56,7 +56,7 @@ private[akkaeffect] object Serially {
             case s: S.Idle   => (S.Active, start(s.value, t))
             case s: S.Active =>
               val task = (a: A) => for {
-                a <- s.task(a)
+                a <- Concurrent[F].defer(s.task(a))
                 a <- t(a)
               } yield a
               (S.Active(task), Concurrent[F].unit)
