a47feceb0b67e1d4cfe03d23b201b55d63b57941
Ilya Surkov <ilyas9406@yandex.ru>
2022-11-21 18:20:46 +0100

    Update dependencies (#201)
    Co-authored-by: Ilya Surkov <isurkov@evolution.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,24 +2,24 @@ import sbt._
 
 object Dependencies {
   
-  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.11"
-  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "3.0.3"
-  val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.3"
+  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.14"
+  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "3.1.1"
+  val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.4"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "3.0.1"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
   val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.13.2"
   val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
-  val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "1.0.1"
+  val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "1.0.6"
 
   object Cats {
-    private val version = "2.7.0"
+    private val version = "2.9.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
   }
 
   object CatsEffect {
-    private val version = "3.3.7"
+    private val version = "3.4.1"
     val effect = "org.typelevel" %% "cats-effect" % version
   }
 
@@ -35,14 +35,6 @@ object Dependencies {
     val `cluster-sharding`  = "com.typesafe.akka" %% "akka-cluster-sharding"  % version
     val `cluster-tools`     = "com.typesafe.akka" %% "akka-cluster-tools"     % version
     val slf4j               = "com.typesafe.akka" %% "akka-slf4j"             % version
-  }
-
-  object SafeAkka {
-    private val version = "3.0.0"
-    val actor                 = "com.evolutiongaming" %% "safe-actor"               % version
-    val persistence           = "com.evolutiongaming" %% "safe-persistence"         % version
-    val `persistence-async`   = "com.evolutiongaming" %% "safe-persistence-async"   % version
-    val `persistence-testkit` = "com.evolutiongaming" %% "safe-persistence-testkit" % version
   }
 
   object Logback {
