304f6412aa7188086ff7cc12e7b4e55790a72751
Ilya Surkov <isurkov@evolution.com>
2022-11-21 17:29:10 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,24 +2,24 @@ import sbt._
 
 object Dependencies {
   
-  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.11"
-  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.7.7"
-  val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.3"
+  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.14"
+  val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "2.9.0"
+  val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.4"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "2.1.1"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
   val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.13.2"
   val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
-  val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "0.3.4"
+  val smetrics                    = "com.evolutiongaming"   %% "smetrics"                  % "0.3.6"
 
   object Cats {
-    private val version = "2.7.0"
+    private val version = "2.9.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
   }
 
   object CatsEffect {
-    private val version = "2.5.4"
+    private val version = "2.5.5"
     val effect = "org.typelevel" %% "cats-effect" % version
   }
 
