3cdc0b4d1efce498cb9da6fbf8eac473ba48191d
Scala Steward <scala_steward@virtuslab.com>
2022-12-20 15:24:24 +0000

    Update retry to 3.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.14"
   val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "3.1.1"
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.4"
-  val retry                       = "com.evolutiongaming"   %% "retry"                     % "3.0.1"
+  val retry                       = "com.evolutiongaming"   %% "retry"                     % "3.0.2"
   val `akka-persistence-inmemory` = "com.github.dnvriend"   %% "akka-persistence-inmemory" % "2.5.15.2"
   val `kind-projector`            = "org.typelevel"          % "kind-projector"            % "0.13.2"
   val pureconfig                  = "com.github.pureconfig" %% "pureconfig"                % "0.12.3"
