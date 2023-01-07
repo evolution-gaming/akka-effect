2b855dc7766b6207d62f07ef75950e7d88341aa6
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 17:40:29 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   
-  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.14"
+  val scalatest                   = "org.scalatest"         %% "scalatest"                 % "3.2.15"
   val `cats-helper`               = "com.evolutiongaming"   %% "cats-helper"               % "3.1.1"
   val `executor-tools`            = "com.evolutiongaming"   %% "executor-tools"            % "1.0.4"
   val retry                       = "com.evolutiongaming"   %% "retry"                     % "3.0.1"
