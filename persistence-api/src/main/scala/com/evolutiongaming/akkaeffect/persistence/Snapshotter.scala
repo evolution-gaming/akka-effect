e6f1680ba758bf33db8f6f2fe969e5830233344a
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-12-11 12:05:26 +0100

    fix minor code review comments

--- persistence-api/src/main/scala/com/evolutiongaming/akkaeffect/persistence/Snapshotter.scala
+++ persistence-api/src/main/scala/com/evolutiongaming/akkaeffect/persistence/Snapshotter.scala
@@ -8,30 +8,31 @@ import com.evolutiongaming.catshelper.{Log, MeasureDuration, MonadThrowable}
 
 import java.time.Instant
 
-/**
-  * Describes communication with underlying snapshot storage
+/** Describes communication with underlying snapshot storage
   *
-  * @tparam A - snapshot
+  * @tparam A
+  *   \- snapshot
   */
 trait Snapshotter[F[_], -A] {
 
-  /**
-    * Saves a `snapshot` of this snapshotter' state.
+  /** Saves a `snapshot` of this snapshotter' state.
     *
-    * @return outer F[_] is about saving in background, inner F[_] is about saving completed
+    * @return
+    *   outer F[_] is about saving in background, inner F[_] is about saving completed
     */
   def save(seqNr: SeqNr, snapshot: A): F[F[Instant]]
 
-  /**
-    * Deletes the snapshot identified by `sequenceNr`.
+  /** Deletes the snapshot identified by `sequenceNr`.
     *
-    * @return outer F[_] is about deletion in background, inner F[_] is about deletion being completed
+    * @return
+    *   outer F[_] is about deletion in background, inner F[_] is about deletion being completed
     */
   def delete(seqNr: SeqNr): F[F[Unit]]
 
-  /**
-    * @see [[akka.persistence.Snapshotter.deleteSnapshots]]
-    * @return outer F[_] is about deletion in background, inner F[_] is about deletion being completed
+  /** @see
+    *   [[akka.persistence.Snapshotter.deleteSnapshots]]
+    * @return
+    *   outer F[_] is about deletion in background, inner F[_] is about deletion being completed
     */
   @deprecated(
     "use Snapshotter.delete(Snapshotter.Criteria) instead",
@@ -39,20 +40,22 @@ trait Snapshotter[F[_], -A] {
   )
   def delete(criteria: SnapshotSelectionCriteria): F[F[Unit]]
 
-  /**
-    * Deletes all snapshots matching `criteria`.
+  /** Deletes all snapshots matching `criteria`.
     *
-    * @return outer F[_] is about deletion in background, inner F[_] is about deletion being completed
+    * @return
+    *   outer F[_] is about deletion in background, inner F[_] is about deletion being completed
     */
   def delete(criteria: Snapshotter.Criteria): F[F[Unit]]
 }
 
 object Snapshotter {
 
-  final case class Criteria(maxSequenceNr: Long = Long.MaxValue,
-                            maxTimestamp: Long = Long.MaxValue,
-                            minSequenceNr: Long = 0L,
-                            minTimestamp: Long = 0L) {
+  final case class Criteria(
+    maxSequenceNr: Long = Long.MaxValue,
+    maxTimestamp: Long = Long.MaxValue,
+    minSequenceNr: Long = 0L,
+    minTimestamp: Long = 0L
+  ) {
     def asAkka: SnapshotSelectionCriteria =
       SnapshotSelectionCriteria(
         maxSequenceNr,
@@ -72,8 +75,7 @@ object Snapshotter {
       )
   }
 
-  def const[F[_], A](instant: F[F[Instant]],
-                     unit: F[F[Unit]]): Snapshotter[F, A] =
+  def const[F[_], A](instant: F[F[Instant]], unit: F[F[Unit]]): Snapshotter[F, A] =
     new Snapshotter[F, A] {
 
       def save(seqNr: SeqNr, snapshot: A) = instant
@@ -85,22 +87,20 @@ object Snapshotter {
       def delete(criteria: Criteria): F[F[Unit]] = unit
     }
 
-  def empty[F[_]: Applicative, A]: Snapshotter[F, A] = {
+  def empty[F[_]: Applicative, A]: Snapshotter[F, A] =
     const(Instant.ofEpochMilli(0L).pure[F].pure[F], ().pure[F].pure[F])
-  }
 
-  private sealed abstract class Convert
+  sealed abstract private class Convert
 
-  private sealed abstract class MapK
+  sealed abstract private class MapK
 
-  private sealed abstract class WithFail
+  sealed abstract private class WithFail
 
-  private sealed abstract class WithLogging
+  sealed abstract private class WithLogging
 
-  implicit class SnapshotterOps[F[_], A](val self: Snapshotter[F, A])
-      extends AnyVal {
+  implicit class SnapshotterOps[F[_], A](val self: Snapshotter[F, A]) extends AnyVal {
 
-    def mapK[G[_]: Applicative](f: F ~> G): Snapshotter[G, A] = {
+    def mapK[G[_]: Applicative](f: F ~> G): Snapshotter[G, A] =
       new MapK with Snapshotter[G, A] {
 
         def save(seqNr: SeqNr, snapshot: A) =
@@ -120,9 +120,8 @@ object Snapshotter {
             f(a)
           }
       }
-    }
 
-    def convert[B](f: B => F[A])(implicit F: FlatMap[F]): Snapshotter[F, B] = {
+    def convert[B](f: B => F[A])(implicit F: FlatMap[F]): Snapshotter[F, B] =
       new Convert with Snapshotter[F, B] {
 
         def save(seqNr: SeqNr, snapshot: B) = f(snapshot).flatMap { a =>
@@ -136,87 +135,71 @@ object Snapshotter {
 
         def delete(criteria: Criteria): F[F[Unit]] = self.delete(criteria)
       }
-    }
 
-    def withLogging1(log: Log[F])(
-      implicit
+    def withLogging1(log: Log[F])(implicit
       F: FlatMap[F],
       measureDuration: MeasureDuration[F]
-    ): Snapshotter[F, A] = {
+    ): Snapshotter[F, A] =
       new WithLogging with Snapshotter[F, A] {
 
-        def save(seqNr: SeqNr, snapshot: A) = {
+        def save(seqNr: SeqNr, snapshot: A) =
           for {
             d <- MeasureDuration[F].start
             r <- self.save(seqNr, snapshot)
-          } yield
-            for {
-              r <- r
-              d <- d
-              _ <- log.info(s"save snapshot at $seqNr in ${d.toMillis}ms")
-            } yield r
-        }
+          } yield for {
+            r <- r
+            d <- d
+            _ <- log.info(s"save snapshot at $seqNr in ${d.toMillis}ms")
+          } yield r
 
-        def delete(seqNr: SeqNr) = {
+        def delete(seqNr: SeqNr) =
           for {
             d <- MeasureDuration[F].start
             r <- self.delete(seqNr)
-          } yield
-            for {
-              r <- r
-              d <- d
-              _ <- log.info(s"delete snapshot at $seqNr in ${d.toMillis}ms")
-            } yield r
-        }
+          } yield for {
+            r <- r
+            d <- d
+            _ <- log.info(s"delete snapshot at $seqNr in ${d.toMillis}ms")
+          } yield r
 
-        def delete(criteria: SnapshotSelectionCriteria) = {
+        def delete(criteria: SnapshotSelectionCriteria) =
           delete(Criteria(criteria))
-        }
 
-        def delete(criteria: Criteria): F[F[Unit]] = {
+        def delete(criteria: Criteria): F[F[Unit]] =
           for {
             d <- MeasureDuration[F].start
             r <- self.delete(criteria)
-          } yield
-            for {
-              r <- r
-              d <- d
-              _ <- log.info(
-                s"delete snapshots for $criteria in ${d.toMillis}ms"
-              )
-            } yield r
-        }
+          } yield for {
+            r <- r
+            d <- d
+            _ <- log.info(
+              s"delete snapshots for $criteria in ${d.toMillis}ms"
+            )
+          } yield r
 
       }
-    }
 
-    def withFail(fail: Fail[F])(implicit
-                                F: MonadThrowable[F]): Snapshotter[F, A] = {
+    def withFail(fail: Fail[F])(implicit F: MonadThrowable[F]): Snapshotter[F, A] =
       new WithFail with Snapshotter[F, A] {
 
-        def save(seqNr: SeqNr, snapshot: A) = {
+        def save(seqNr: SeqNr, snapshot: A) =
           fail.adapt(s"failed to save snapshot at $seqNr") {
             self.save(seqNr, snapshot)
           }
-        }
 
-        def delete(seqNr: SeqNr) = {
+        def delete(seqNr: SeqNr) =
           fail.adapt(s"failed to delete snapshot at $seqNr") {
             self.delete(seqNr)
           }
-        }
 
-        def delete(criteria: SnapshotSelectionCriteria) = {
+        def delete(criteria: SnapshotSelectionCriteria) =
           delete(Criteria(criteria))
-        }
 
-        def delete(criteria: Criteria): F[F[Unit]] = {
+        def delete(criteria: Criteria): F[F[Unit]] =
           fail.adapt(s"failed to delete snapshots for $criteria") {
             self.delete(criteria)
           }
-        }
 
       }
-    }
   }
 }
