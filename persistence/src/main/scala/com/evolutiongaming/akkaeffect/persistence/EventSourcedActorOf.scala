1dfa737ccdb167f2a6c492a1e29106214011eb3b
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2024-02-26 12:40:07 +0100

    log any errors in EventSourcedActorOf receive allocation

--- persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourcedActorOf.scala
+++ persistence/src/main/scala/com/evolutiongaming/akkaeffect/persistence/EventSourcedActorOf.scala
@@ -67,95 +67,105 @@ object EventSourcedActorOf {
     eventSourcedOf: EventSourcedOf[F, Lifecycle[F, S, E, C]],
     persistence: EventSourcedPersistence[F]
   ): ReceiveOf[F, Envelope[Any], ActorOf.Stop] = { actorCtx =>
-    for {
-      log <- LogOf.log[F, EventSourcedActorOf.type].toResource
-      log <- log.prefixed(actorCtx.self.path.name).pure[Resource[F, *]]
-      lor <- log.mapK(Resource.liftK[F]).pure[Resource[F, *]]
-
-      eventSourced    <- eventSourcedOf(actorCtx).toResource
-      recoveryStarted <- eventSourced.value
-
-      snapshotStore <- persistence.snapshotStore[S](eventSourced).toResource
-      eventStore    <- persistence.eventStore[E](eventSourced).toResource
-
-      snapshot <- snapshotStore.latest.toResource
-      _        <- lor.debug(s"using snapshot $snapshot")
-
-      recovering <- recoveryStarted(
-        snapshot.map(_.metadata.seqNr).getOrElse(SeqNr.Min),
-        snapshot.map(_.asOffer)
-      )
-      _ <- lor.debug(s"snapshot applied, continue with events")
-
-      replay = for {
-        replay <- recovering.replay
-        _      <- lor.debug(s"replay allocated")
-      } yield replay
-
-      seqNr <- replay.use { replay =>
-        val snapSeqNr = snapshot.map(_.metadata.seqNr).getOrElse(SeqNr.Min)
-        val fromSeqNr = snapshot.map(_.metadata.seqNr + 1).getOrElse(SeqNr.Min)
-        for {
-          _      <- log.debug(s"snapshot seqNr: $snapSeqNr, load events from seqNr: $fromSeqNr")
-          events <- eventStore.events(fromSeqNr)
-          _      <- log.debug(s"events stream created")
-          seqNrL <- events.foldWhileM(snapSeqNr) {
-            case (_, EventStore.Event(event, seqNr)) => replay(event, seqNr).as(seqNr.asLeft[Unit])
-            case (_, EventStore.HighestSeqNr(seqNr)) => seqNr.asLeft[Unit].pure[F]
-          }
-          _ <- log.debug(s"events applied, resulting in seqNr $seqNrL")
-          seqNr <- seqNrL match {
-            case Left(seqNr) => seqNr.pure[F]
-            case Right(_)    =>
-              // function used in events.foldWhileM always returns Left
-              // and sstream.Stream.foldWhileM returns Right only if passed function do so
-              // thus makes getting Right here impossible
-              new IllegalStateException("should never happened").raiseError[F, SeqNr]
-          }
-        } yield seqNr
-      }.toResource
-
-      currentSeqNr <- Ref[F].of(seqNr).toResource
-
-      snapshotter = new Snapshotter[F, S] {
-
-        def save(seqNr: SeqNr, snapshot: S): F[F[Instant]] = snapshotStore.save(seqNr, snapshot)
-
-        def delete(seqNr: SeqNr): F[F[Unit]] = snapshotStore.delete(seqNr)
-
-        def delete(criteria: SnapshotSelectionCriteria): F[F[Unit]] = snapshotStore.delete(criteria.asStoreCriteria)
-
+    LogOf
+      .log[F, EventSourcedActorOf.type]
+      .toResource
+      .map { log =>
+        log
+          .prefixed(actorCtx.self.path.name)
+          .mapK(Resource.liftK[F])
       }
+      .flatMap { implicit log =>
+        val receive = for {
+          eventSourced    <- eventSourcedOf(actorCtx).toResource
+          recoveryStarted <- eventSourced.value
 
-      journaller = new Journaller[F, E] {
+          snapshotStore <- persistence.snapshotStore[S](eventSourced).toResource
+          eventStore    <- persistence.eventStore[E](eventSourced).toResource
 
-        val append = new Append[F, E] {
+          snapshot <- snapshotStore.latest.toResource
+          _        <- log.debug(s"using snapshot $snapshot")
 
-          def apply(events: Events[E]): F[F[SeqNr]] =
-            currentSeqNr
-              .modify { seqNr0 =>
-                events.mapAccumulate(seqNr0) {
-                  case (seqNr0, event) =>
-                    val seqNr1 = seqNr0 + 1
-                    seqNr1 -> EventStore.Event(event, seqNr1)
-                }
+          recovering <- recoveryStarted(
+            snapshot.map(_.metadata.seqNr).getOrElse(SeqNr.Min),
+            snapshot.map(_.asOffer)
+          )
+          _ <- log.debug(s"snapshot applied, continue with events")
+
+          replay = for {
+            replay <- recovering.replay
+            _      <- log.debug(s"replay allocated")
+          } yield replay
+
+          seqNr <- replay.use { replay =>
+            val snapSeqNr = snapshot.map(_.metadata.seqNr).getOrElse(SeqNr.Min)
+            val fromSeqNr = snapshot.map(_.metadata.seqNr + 1).getOrElse(SeqNr.Min)
+            for {
+              _      <- log.debug(s"snapshot seqNr: $snapSeqNr, load events from seqNr: $fromSeqNr").allocated
+              events <- eventStore.events(fromSeqNr)
+              _      <- log.debug(s"events stream created").allocated
+              seqNrL <- events.foldWhileM(snapSeqNr) {
+                case (_, EventStore.Event(event, seqNr)) => replay(event, seqNr).as(seqNr.asLeft[Unit])
+                case (_, EventStore.HighestSeqNr(seqNr)) => seqNr.asLeft[Unit].pure[F]
               }
-              .flatMap { events =>
-                eventStore.save(events)
+              _ <- log.debug(s"events applied, resulting in seqNr $seqNrL").allocated
+              seqNr <- seqNrL match {
+                case Left(seqNr) => seqNr.pure[F]
+                case Right(_)    =>
+                  // function used in events.foldWhileM always returns Left
+                  // and sstream.Stream.foldWhileM returns Right only if passed function do so
+                  // thus makes getting Right here impossible
+                  new IllegalStateException("should never happened").raiseError[F, SeqNr]
               }
+            } yield seqNr
+          }.toResource
 
+          currentSeqNr <- Ref[F].of(seqNr).toResource
+
+          snapshotter = new Snapshotter[F, S] {
+
+            def save(seqNr: SeqNr, snapshot: S): F[F[Instant]] = snapshotStore.save(seqNr, snapshot)
+
+            def delete(seqNr: SeqNr): F[F[Unit]] = snapshotStore.delete(seqNr)
+
+            def delete(criteria: SnapshotSelectionCriteria): F[F[Unit]] = snapshotStore.delete(criteria.asStoreCriteria)
+
+          }
+
+          journaller = new Journaller[F, E] {
+
+            val append = new Append[F, E] {
+
+              def apply(events: Events[E]): F[F[SeqNr]] =
+                currentSeqNr
+                  .modify { seqNr0 =>
+                    events.mapAccumulate(seqNr0) {
+                      case (seqNr0, event) =>
+                        val seqNr1 = seqNr0 + 1
+                        seqNr1 -> EventStore.Event(event, seqNr1)
+                    }
+                  }
+                  .flatMap { events =>
+                    eventStore.save(events)
+                  }
+
+            }
+
+            val deleteTo = new DeleteEventsTo[F] {
+
+              def apply(seqNr: SeqNr): F[F[Unit]] = eventStore.deleteTo(seqNr)
+
+            }
+
+          }
+
+          receive <- recovering.completed(seqNr, journaller, snapshotter)
+        } yield receive.contramapM[Envelope[Any]](_.cast[F, C])
+
+        receive.onError {
+          case err: Throwable => log.error(s"failed to allocate receive", err)
         }
-
-        val deleteTo = new DeleteEventsTo[F] {
-
-          def apply(seqNr: SeqNr): F[F[Unit]] = eventStore.deleteTo(seqNr)
-
-        }
-
       }
-
-      receive <- recovering.completed(seqNr, journaller, snapshotter)
-    } yield receive.contramapM[Envelope[Any]](_.cast[F, C])
   }
 
   implicit private class SnapshotOps[S](val snapshot: SnapshotStore.Offer[S]) extends AnyVal {
