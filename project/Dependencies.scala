767b4f3dcfda747738ab8a35dd1951ceb0031867
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-03-28 17:28:41 +0200

    update logback to 1.5.3 and slf4j to 2.0.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,13 +39,13 @@ object Dependencies {
   }
 
   object Logback {
-    private val version = "1.2.11"
+    private val version = "1.5.3"
     val core    = "ch.qos.logback" % "logback-core"    % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
 
   object Slf4j {
-    private val version = "1.7.36"
+    private val version = "2.0.12"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
