d888f0f8326c919a2be05f1fff996529ba4fb948
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-04-30 15:19:43 +0300

    update Cats Effect to 3.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   }
 
   object CatsEffect {
-    private val version = "3.4.11"
+    private val version = "3.5.4"
     val effect          = "org.typelevel" %% "cats-effect" % version
   }
 
