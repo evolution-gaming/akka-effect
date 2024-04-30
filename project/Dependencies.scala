92f4c5df420b1556fc7e75eb3f3de9d0d63ac084
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-04-30 15:20:35 +0300

    update logback to 1.5.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   }
 
   object Logback {
-    private val version = "1.5.3"
+    private val version = "1.5.6"
     val core            = "ch.qos.logback" % "logback-core"    % version
     val classic         = "ch.qos.logback" % "logback-classic" % version
   }
