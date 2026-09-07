234458327bfd3dfd9b7517a32f7c6f690d5c8534
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-07 09:49:53 +0200

    Update log4j-over-slf4j, slf4j-api to 2.0.19 (#524)
    ## About this PR
    📦 Updates 
    * org.slf4j:log4j-over-slf4j
    * org.slf4j:slf4j-api
    
     from `2.0.18` to `2.0.19`
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/766c7e43b6597fdbcc6378030a62f441786947f5/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "org.slf4j" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "org.slf4j" }
    }]
    ```
    </details>
    
    <sup>
    labels: test-library-update, early-semver-patch, semver-spec-patch,
    commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForGroupId" : {
          "forArtifactIds" : [
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "org.slf4j",
                    "artifactId" : {
                      "name" : "log4j-over-slf4j",
                      "maybeCrossName" : null
                    },
                    "version" : "2.0.18",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : "test"
                  }
                ],
                "newerVersions" : [
                  "2.0.19"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            },
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "org.slf4j",
                    "artifactId" : {
                      "name" : "slf4j-api",
                      "maybeCrossName" : null
                    },
                    "version" : "2.0.18",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : "test"
                  }
                ],
                "newerVersions" : [
                  "2.0.19"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            }
          ]
        }
      },
      "Labels" : [
        "test-library-update",
        "early-semver-patch",
        "semver-spec-patch",
        "commit-count:1"
      ]
    } -->

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
   }
 
   object Slf4j {
-    private val version    = "2.0.18"
+    private val version    = "2.0.19"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
